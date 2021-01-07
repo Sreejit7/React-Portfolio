65f58d7679e6a525756cc66bc0a3f8adda950afb
Sreejit7 <sreejitde7@gmail.com>
2021-01-07 21:45:58 +0530

    Updated about

--- src/About.js
+++ src/About.js
@@ -35,13 +35,13 @@ function About() {
       a: `I'm a Final Year B.Tech Student in Computer Science from Tamluk, India. 
           I love problem solving and web development. 
           I'm always looking forward to learn new technologies and evolving my skills constantly.
-          Currently learning JavaScript & Mongo DB!`
+          Currently learning JavaScript & Mongo DB! I'm about to graduate in 2021, so I'm looking for my first SDE Internship and/or Full-time role, please drop a message if you have an open position or know someone who might. Thanks!`
     },
     {
       q: "My Coding Journey",
       a:  `I'm one of those who never coded before college and started really late.
           (Better late than never!)
-          The first language I was introduced to at my college was C. I know coding can be equally fun and frustrating at times,
+          The first language I was introduced to at my college was C. Since then, I've learnt a few new languages and frameworks, and each one has added to the excitement of learning something new! I know coding can be equally fun and frustrating at times,
           but when it works, that gives me great inner joy! 
           The world of programming is a never-ending ocean, and I hope to explore it a lil deeper a day.`,
     },
