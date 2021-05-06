243ef329aaadbe1b8f017c505dfb11a8babb7241
Sreejit De <sreejitde7@gmail.com>
2021-05-06 20:01:51 +0530

    Updated Resume link (#2)
    * Updated Resume link
    
    Co-authored-by: Sreejit De <sreejitde7@gmil.com>

--- src/SidebarItems.js
+++ src/SidebarItems.js
@@ -13,7 +13,7 @@ export const sidebarItems = [
   },
   {
     name: 'RESUME',
-    link: 'https://drive.google.com/file/d/1S6gFtxDJhafDHRNgEi4VKUPW878yOJh8/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/1lF9QU7aX0BX1MEk1yGh3WGRq2AXIUd6C/view?usp=sharing',
   },
   {
     name: 'CONTACT ME',
